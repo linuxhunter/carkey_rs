1ab54d18a8fee416838d663634f03bc42b916a60
david <linuxhunter80@gmail.com>
2023-09-18 10:09:28 +0800

    add error handling on spake2+

--- src/iccoa/pairing.rs
+++ src/iccoa/pairing.rs
@@ -33,82 +33,63 @@ lazy_static! {
     static ref PAIRING_CA: Mutex<Vec<u8>> = Mutex::new(vec![]);
 }
 
-pub fn calculate_p_b() -> BigNum {
+pub fn calculate_p_b() -> Result<BigNum> {
     let nid = Nid::X9_62_PRIME256V1;
-    let group = EcGroup::from_curve_name(nid).unwrap();
-    let mut ctx = BigNumContext::new().unwrap();
+    let group = EcGroup::from_curve_name(nid).map_err(|_| ErrorKind::ICCOAPairingError("create prime 256 v1 ec group error".to_string()))?;
+    let mut ctx = BigNumContext::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num context error".to_string()))?;
 
     //calculate base point P
     let base_point = group.generator();
-    let mut x = BigNum::new().unwrap();
-    let mut y = BigNum::new().unwrap();
-    base_point.affine_coordinates_gfp(&group, &mut x, &mut y, &mut ctx).unwrap();
-    let mut p = P.lock().unwrap();
+    let mut x = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("crate big number x error".to_string()))?;
+    let mut y = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create big number y error".to_string()))?;
+    base_point.affine_coordinates_gfp(&group, &mut x, &mut y, &mut ctx).map_err(|_| ErrorKind::ICCOAPairingError("get affine coorderinate prime on ec group error".to_string()))?;
+    let mut p = P.lock().map_err(|_| ErrorKind::ICCOAPairingError("lock static global P variable error".to_string()))?;
     *p = x;
 
     //calculate random u32 y
     let mut rng = rand::thread_rng();
     let y = rng.gen::<u32>();
-    let mut random_y = RANDOM_Y.lock().unwrap();
+    let mut random_y = RANDOM_Y.lock().map_err(|_| ErrorKind::ICCOAPairingError("get random u32 integer error".to_string()))?;
     *random_y = y;
 
     //calculate p_b
-    let mut tmp_y = BigNum::new().unwrap();
-    tmp_y.checked_mul(&*p, &BigNum::from_u32(y).unwrap(), &mut ctx).unwrap();
+    let mut tmp_y = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    tmp_y.checked_mul(&*p, &BigNum::from_u32(y).unwrap(), &mut ctx).map_err(|_| ErrorKind::ICCOAPairingError("multipy big number y with P error".to_string()))?;
 
-    let mut tmp_n = BigNum::new().unwrap();
-    tmp_n.checked_mul(&N, &W0, &mut ctx).unwrap();
+    let mut tmp_n = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    tmp_n.checked_mul(&N, &W0, &mut ctx).map_err(|_| ErrorKind::ICCOAPairingError("multipy N with W0 error".to_string()))?;
 
-    let mut p_b = BigNum::new().unwrap();
-    p_b.checked_add(&tmp_y, &tmp_n).unwrap();
-    p_b
+    let mut p_b = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    p_b.checked_add(&tmp_y, &tmp_n).map_err(|_| ErrorKind::ICCOAPairingError("add big number y*P and N*W0 error".to_string()))?;
+    Ok(p_b)
 }
 
-pub fn verify_p_a() {
+pub fn calculate_z_v() -> Result<()> {
     let h = 1;
-    let y = *RANDOM_Y.lock().unwrap();
-    let mut ctx = BigNumContext::new().unwrap();
+    let y = *RANDOM_Y.lock().map_err(|_| ErrorKind::ICCOAPairingError("get random u32 integer error".to_string()))?;
+    let mut ctx = BigNumContext::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num context error".to_string()))?;
 
-    let mut tmp_m = BigNum::new().unwrap();
-    tmp_m.checked_mul(&W0, &M, &mut ctx).unwrap();
+    let mut tmp_m = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    tmp_m.checked_mul(&W0, &M, &mut ctx).map_err(|_| ErrorKind::ICCOAPairingError("multipy big number W0 and M error".to_string()))?;
 
     let p_a = PAIRING_PA.lock().unwrap();
-    let mut tmp_z = BigNum::new().unwrap();
-    tmp_z.checked_sub(&*p_a, &tmp_m).unwrap();
+    let mut tmp_z = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    tmp_z.checked_sub(&*p_a, &tmp_m).map_err(|_| ErrorKind::ICCOAPairingError("substract PA with W0*M error".to_string()))?;
 
-    let mut ec_z = BigNum::new().unwrap();
-    ec_z.checked_mul(&BigNum::from_u32(h*y).unwrap(), &tmp_z, &mut ctx).unwrap();
-    let mut ec_v = BigNum::new().unwrap();
-    ec_v.checked_mul(&L, &BigNum::from_u32(h*y).unwrap(), &mut ctx).unwrap();
+    let mut ec_z = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    ec_z.checked_mul(&BigNum::from_u32(h*y).unwrap(), &tmp_z, &mut ctx).map_err(|_| ErrorKind::ICCOAPairingError("multipy h*y with z error".to_string()))?;
+    let mut ec_v = BigNum::new().map_err(|_| ErrorKind::ICCOAPairingError("create openssl big num error".to_string()))?;
+    ec_v.checked_mul(&L, &BigNum::from_u32(h*y).unwrap(), &mut ctx).map_err(|_| ErrorKind::ICCOAPairingError("multipy L with h*y error".to_string()))?;
 
-    let mut pairing_z = PAIRING_Z.lock().unwrap();
+    let mut pairing_z = PAIRING_Z.lock().map_err(|_| ErrorKind::ICCOAPairingError("get global static pairing Z error".to_string()))?;
     *pairing_z = ec_z;
-    let mut paring_v = PAIRING_V.lock().unwrap();
+    let mut paring_v = PAIRING_V.lock().map_err(|_| ErrorKind::ICCOAPairingError("get global static pairing V error".to_string()))?;
     *paring_v = ec_v;
+    Ok(())
 }
 
 pub fn calculate_p_a() -> [u8; 65] {
     [0x00; 65]
-}
-
-pub fn set_pairing_key_mac(key: &[u8]) {
-    let mut pairing_key = PAIRING_KEY.lock().unwrap();
-    pairing_key.set_key_mac(key);
-}
-
-pub fn set_pairing_key_enc(key: &[u8]) {
-    let mut pairing_key = PAIRING_KEY.lock().unwrap();
-    pairing_key.set_key_enc(key);
-}
-
-pub fn get_pairing_key_mac() -> Vec<u8> {
-    let pairing_key = PAIRING_KEY.lock().unwrap();
-    pairing_key.get_key_mac()
-}
-
-pub fn get_pairing_key_enc() -> Vec<u8> {
-    let pairing_key = PAIRING_KEY.lock().unwrap();
-    pairing_key.get_key_enc()
 }
 
 pub fn calculate_length_element_tt(element: &[u8]) -> Vec<u8> {
@@ -131,7 +112,7 @@ pub fn calculate_tt() -> Vec<u8> {
     tt
 }
 
-pub fn calculate_c_b() -> [u8; 16] {
+pub fn calculate_c_b() -> Result<[u8; 16]> {
     let tt = calculate_tt();
     let hash_tt = utils::calculate_sha256(&tt);
     let k_a = &hash_tt[0..16];
@@ -139,17 +120,38 @@ pub fn calculate_c_b() -> [u8; 16] {
     let derived_key = utils::calculate_derive_key(None, k_a, "ConfirmationKeys".as_bytes(), 32);
     let k_ca = &derived_key[0..16];
     let k_cb = &derived_key[16..32];
-    let p_a = PAIRING_PA.lock().unwrap();
-    let p_b = PAIRING_PB.lock().unwrap();
-    let c_a = utils::calculate_cmac(k_ca, &p_b.to_vec()).unwrap();
-    let c_b = utils::calculate_cmac(k_cb, &p_a.to_vec()).unwrap();
-    let mut pairing_c_a = PAIRING_CA.lock().unwrap();
+    let p_a = PAIRING_PA.lock().map_err(|_| ErrorKind::ICCOAPairingError("get global static PA error".to_string()))?;
+    let p_b = PAIRING_PB.lock().map_err(|_| ErrorKind::ICCOAPairingError("get global static PB error".to_string()))?;
+    let c_a = utils::calculate_cmac(k_ca, &p_b.to_vec()).map_err(|_| ErrorKind::ICCOAPairingError("calculate cmac on PB with k_ca error".to_string()))?;
+    let c_b = utils::calculate_cmac(k_cb, &p_a.to_vec()).map_err(|_| ErrorKind::ICCOAPairingError("calculate cmakc on PA with k_cb error".to_string()))?;
+    let mut pairing_c_a = PAIRING_CA.lock().map_err(|_| ErrorKind::ICCOAPairingError("get global static CA error".to_string()))?;
     *pairing_c_a = c_a.clone();
-    c_b.try_into().unwrap()
+    c_b.try_into().map_err(|_| ErrorKind::ICCOAPairingError("get c_b array error".to_string()).into())
 }
 
 pub fn calculate_c_a() -> [u8; 16] {
     [0x00; 16]
+}
+
+
+pub fn set_pairing_key_mac(key: &[u8]) {
+    let mut pairing_key = PAIRING_KEY.lock().unwrap();
+    pairing_key.set_key_mac(key);
+}
+
+pub fn set_pairing_key_enc(key: &[u8]) {
+    let mut pairing_key = PAIRING_KEY.lock().unwrap();
+    pairing_key.set_key_enc(key);
+}
+
+pub fn get_pairing_key_mac() -> Vec<u8> {
+    let pairing_key = PAIRING_KEY.lock().unwrap();
+    pairing_key.get_key_mac()
+}
+
+pub fn get_pairing_key_enc() -> Vec<u8> {
+    let pairing_key = PAIRING_KEY.lock().unwrap();
+    pairing_key.get_key_enc()
 }
 
 pub fn get_vehicle_certificate() -> Vec<u8> {
@@ -190,7 +192,7 @@ pub fn get_carkey_certificate() -> Vec<u8> {
 
 pub fn create_iccoa_pairing_data_request_package() -> Result<Vec<u8>> {
     let transaction_id = 0x0000;
-    let p_b = calculate_p_b();
+    let p_b = calculate_p_b()?;
     let p_b_payload = TLVPayloadBuilder::new().set_tag(0x51).set_value(&p_b.to_vec()).build();
     let mut pb = PAIRING_PB.lock().unwrap();
     *pb = p_b;
@@ -310,8 +312,8 @@ pub fn handle_iccoa_pairing_p_a_payload(iccoa: &ICCOA) -> Result<()> {
     if p_a_tlv_payload.get_tag() != 0x52 {
         return Err(ErrorKind::ICCOAPairingError("handle pairing pA payload error".to_string()).into());
     }
-    let mut pairing_p_a = PAIRING_PA.lock().unwrap();
-    *pairing_p_a = BigNum::from_slice(&p_a_tlv_payload.value).unwrap();
+    let mut pairing_p_a = PAIRING_PA.lock().map_err(|_| ErrorKind::ICCOAPairingError("get global static PA error".to_string()))?;
+    *pairing_p_a = BigNum::from_slice(&p_a_tlv_payload.value).map_err(|_| ErrorKind::ICCOAPairingError("create Big Number from mobild PA TLV payload error".to_string()))?;
     Ok(())
 }
 
@@ -392,9 +394,9 @@ pub fn handle_iccoa_pairing_response_from_mobile(iccoa: &ICCOA) -> Result<ICCOA>
         0x02 => {   //get pA
             //handle pA
             handle_iccoa_pairing_p_a_payload(iccoa)?;
-            verify_p_a();
+            calculate_z_v()?;
             //create spake2+ auth request cB
-            let c_b = calculate_c_b();
+            let c_b = calculate_c_b()?;
             let c_b_payload = TLVPayloadBuilder::new().set_tag(0x53).set_value(&c_b).build();
             let response = create_iccoa_paring_auth_request(transaction_id, &[c_b_payload])?;
             return Ok(response)
@@ -492,7 +494,7 @@ mod tests {
     #[test]
     fn test_spake2_plus_data_request() {
         let transaction_id = 0x0001;
-        let p_b = calculate_p_b();
+        let p_b = calculate_p_b().unwrap();
         let p_b_payload = TLVPayloadBuilder::new().set_tag(0x51).set_value(&p_b.to_vec()).build();
         let salt = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f];
         let salt_payload = TLVPayloadBuilder::new().set_tag(0xC0).set_value(&salt).build();
@@ -570,7 +572,7 @@ mod tests {
     fn test_spake2_plus_auth_request() {
         let transaction_id = 0x0002;
         calculate_p_a();
-        let c_b = calculate_c_b();
+        let c_b = calculate_c_b().unwrap();
         let c_b_payload = TLVPayloadBuilder::new().set_tag(0x53).set_value(&c_b).build();
         let iccoa = create_iccoa_paring_auth_request(transaction_id, &[c_b_payload]).unwrap();
         assert_eq!(iccoa, ICCOA {
